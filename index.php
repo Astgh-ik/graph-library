35d5c6055bdf37140d0b99c918eb1d4917a2eb28
Astghik <astghik_avetisyan01@mail.ru>
2020-05-05 14:25:26 +0400

    changes

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 class Graph{
 	public	$nodes = [];
 	public $edges = [];
-
 	public function add_node($node){
 		array_push($this->nodes,$node);
 	}
@@ -19,11 +18,11 @@ class Graph{
 			array_push($this->edges,$edges[$i]);
 		}
 	}
-	public function degree($node=0){
+	public function degree($node=0){//գագաթի աստիճան նշանակում է կից կողերի քանակը
 		$degree = 0;
 		if (in_array($node, $this->nodes)) {
 			for($i = 0;$i < count($this->edges);$i++){
-				if (in_array($node,$this->edges[$i])) {
+				if (in_array($node,$this->edges[$i])) {//գտնում ենք կից եզրերը
 					$degree++;
 				}
 			}
@@ -42,8 +41,7 @@ class Graph{
 	public function density(){
 		$node=count($this->nodes);
 		$edge = count($this->edges);
-
-		$density = (2*$edge)/($node*($node-1));
+		$density = (2*$edge)/($node*($node-1));//գրաֆի խտության բանաձև
 		return $density;
 	}
 	public function info($n=0){
@@ -59,9 +57,6 @@ class Graph{
 		$g->nodes = $this->nodes;
 		$g->edges = [];
 		return $g;
-	}
-	public function is_directed(){
-
 	}
 	public function is_empty(){
 		if (count($this->edges) == 0) {
@@ -86,10 +81,9 @@ class Graph{
 	public function number_of_nodes(){
 		return count($this->nodes);
 	}
-
 	public function neighbors($n){
 		$arr = [];
-		for($i = 0;$i < count($this->edges);$i++){
+		for($i = 0;$i < count($this->edges);$i ++){
 			if (in_array($n,$this->edges[$i])) {
 				for($j = 0;$j < 2;$j++){
 					if ($this->edges[$i][$j] != $n) {
@@ -101,22 +95,20 @@ class Graph{
 		return $arr;
 	}
 	public function non_neighbors($n){
-
 		$arr = [];
-		$node=$this->nodes;
-		for($i=0;$i<count($this->edges);$i++){
+		$node = $this->nodes;
+		for($i = 0;$i < count($this->edges);$i++){
 			if (in_array($n,$this->edges[$i])) {
-				for($j = 0;$j<2;$j++){
-					if ($this->edges[$i][$j]!=$n) {
+				for($j = 0;$j < 2;$j++){
+					if ($this->edges[$i][$j] != $n) {
 						array_push($arr,$this->edges[$i][$j]);
 					}
 				}
 			}
 		}
 		for($i = 0;$i < count($arr);$i++){
-			for($j = 0;$j<count($node);$j++){
+			for($j = 0;$j < count($node);$j++){
 				if ($arr[$i] == $node[$j]) {
-					
 					array_splice($node,$j,1);
 				}
 			}
@@ -124,10 +116,10 @@ class Graph{
 		return $node;
 	}
 	public function common_neighbors($u,$v){
-		$arr=[];
-		$u_neighbors=$this->neighbors($u);
-		$v_neighbors=$this->neighbors($v);
-		for($i=0;$i<count($u_neighbors);$i++){
+		$arr = [];
+		$u_neighbors = $this->neighbors($u);
+		$v_neighbors = $this->neighbors($v);
+		for($i = 0;$i < count($u_neighbors);$i++){
 			if (in_array($u_neighbors[$i], $v_neighbors)) {
 				array_push($arr,$u_neighbors[$i]);
 			}
@@ -139,10 +131,6 @@ class Graph{
 	}
 	public function number_of_edges(){
 		return count($this->edges);
-	}
-
-	public function non_edges(){
-
 	}
 	public function number_of_selfloops(){
 		$count = 0;
@@ -172,17 +160,15 @@ class Graph{
 		return $arr;
 	}
 	public function add_star($nodes_for_star){
-
 		$middle = $nodes_for_star[0];
 		for($i = 1;$i < count($nodes_for_star); $i++){
 			if (!in_array($nodes_for_star[$i], $this->nodes)) {
 				array_push($this->nodes,$nodes_for_star[$i]);
 			}
-			$arr=[$middle];
-			array_push($arr,$nodes_for_star[$i]);
+			$arr = [$middle];
+			array_push($arr,$nodes_for_star[$i]);//առաջին գագաթը միացնում է մնացածին
 			array_push($this->edges,$arr);
 		}
-
 	}
 	public function add_path($nodes_for_path){
 		for($i = 0;$i < count($nodes_for_path)-1;$i++){
@@ -194,46 +180,38 @@ class Graph{
 			array_push($arr,$nodes_for_path[$i+1]);
 		}
 	}
-	public function add_cycle($nodes_for_cycle){
-
-
-	}
-
 	public function induced_subgraph($nbunch){
 		$sub = new Graph();
 		$sub->nodes = $nbunch;
 		$edge = [];
-		for($i=0;$i<count($this->edges);$i++){
+		for($i = 0;$i < count($this->edges);$i++){
 			if (in_array($this->edges[$i][0], $nbunch) && in_array($this->edges[$i][1], $nbunch)) {
+				//գտնում է այն եզրերը որոնք կազմված են տրված հանգույցներով
 				array_push($edge,$this->edges[$i]);
 			}
-
 		}
 		$sub->edges = $edge;
 		return $sub;
 	}
-
-// kisat
 	public function remove_node($node){
 		if (!in_array($this->nodes, $node)) {
 			print_r('Նշված գագաթը չկա գրաֆում');
 		}
-		
 		for($i = 0;$i<count($this->nodes);$i++){
 			if ($this->nodes[$i]==$node) {
 				array_splice($this->nodes,$i, 1);
 			}
 		}
-		for($i = 0;$i<count($this->edges);$i++){
+		for($i = 0;$i < count($this->edges);$i++){
 			if (in_array($node, $this->edges[$i])) {
+				//հեռացնում է այն եզրերը որոնք կազմված էին տրված հանգույցով
 				array_splice($this->edges,$i,1);
 				$i--;
 			}
 		}
-
 	}
 	public function remove_edge($u,$v){
-		for($i=0;$i<count($this->edges);$i++){
+		for($i = 0;$i < count($this->edges);$i++){
 			if (in_array($u, $this->edges[$i]) && in_array($v, $this->edges[$i])) {
 				array_splice($this->edges,$i,1);
 			}
@@ -257,74 +235,52 @@ class Graph{
 		}
 		return false;
 	}
-
-
-
-
 	public function min_weighted_vertex_cover(){
-		$arr=[];
+		$arr = [];
 		foreach ($this->edges as $key) {
 			if (!(in_array($key[1], $arr))) {
-				$n=$this->neighbors($key[1]);
-				$c=0;
-				for($i=0;$i<count($n);$i++){
+				$n = $this->neighbors($key[1]);
+				$c = 0;
+				for($i = 0;$i < count($n);$i++){
 					if (in_array($n[$i], $arr)) {
 						$c++;
 					}
 				}
-				if ($c==0) {
+				if ($c == 0) {
 					array_push($arr, $key[1]);
 				}
 				else{
 					if (!(in_array($key[1],$arr)||in_array($key[0],$arr))) {
-						if($c<count($n)){
+						if($c < count($n)){
 							array_push($arr,$key[1]);
 						}
-						else{array_push($arr,$key[0]);}
+						else{
+							array_push($arr,$key[0]);
+						}
 					}
 				}
-
 			}
-
 		}
-		print_r($arr);
 		return $arr;
 	}
 	public function independent_set(){
-		$arr=[];
+		$arr = [];
 		foreach ($this->edges as $key) {
 			if (!(in_array($key[1], $arr))) {
-				$n=$this->neighbors($key[1]);
-				$c=0;
-				for($i=0;$i<count($n);$i++){
+				$n = $this->neighbors($key[1]);
+				$c = 0;
+				for($i = 0;$i < count($n);$i++){
 					if (in_array($n[$i], $arr)) {
 						$c++;
 					}
 				}
-				if ($c==0) {
+				if ($c == 0) {
 					array_push($arr, $key[1]);
 				}
 			}
 		}
 		return $arr;
 	}
-		
-	public function min_mximal_matching(){
-		$arr=[];
-		array_push($arr,$this->edges[0]);
-		foreach ($this->edges as $key) {
-			for($i=0;$i<count($arr);$i++){
-			if (!in_array($key[0],$arr[$i])&&!in_array($key[1], $arr[$i])) {
-				print "<pre>";
-		print_r(count($arr));
-				array_push($arr, $key);
-			}
-		}
-	}
-	return $arr;
-	}
-
-
 }
 $G = new Graph();
 $G->add_nodes([1,2,3,4,5,6]);
